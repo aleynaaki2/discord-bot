1d13280de1eaea59db98010bd89fe636c2d57aa2
Tomris <tomris@Tomris-Dizustu-Bilgisayar.local>
2023-10-31 11:13:15 +0300

    kedy

--- src/index.js
+++ src/index.js
@@ -11,33 +11,10 @@ const client = new Client({
   ]
 });
 
-//Music
-const { YTSearcher } = require('ytsearcher');
-const ytdl = require('ytdl-core-discord');
-const ytApiKey = 'AIzaSyCa0uZ7hAoDmf7EWzZkvRacPVE4YHdZP-8';
-const searcher = new YTSearcher(ytApiKey);
-
 client.on('ready', () => {
   console.log(`🥧 ${client.user.tag} is online`);
 });
 
-client.on('messageCreate', async (message) => {
-  if (message.content.startsWith('!play')) {
-    const query = message.content.slice(6);
-    const video = await searcher.search(query);
 
-    if (video && video.videos.length > 0) {
-      const connection = await message.member.voice.channel.join();
-      const stream = ytdl(video.videos[0].url, { filter: 'audioonly' });
-      const dispatcher = connection.play(stream);
-
-      dispatcher.on('finish', () => {
-        connection.disconnect();
-      });
-    } else {
-      message.channel.send('Video not found!');
-    }
-  }
-});
 // test degisikligi
 client.login(process.env.TOKEN);
